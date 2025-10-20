a6b28e9cbf4a36d709ba281b4a6fe81278f60adf
fan <291950304@qq.com>
2025-10-20 13:43:29 +0800

    chore: go hello

--- code/learn-go-with-tests/01_hello/hello.go
+++ code/learn-go-with-tests/01_hello/hello.go
@@ -3,14 +3,31 @@ package main
 import "fmt"
 
 const englishPrefix = "Hello "
+const chinesePrefix = "你好 "
+const spanishPrefix = "Hola "
+const frenchPrefix = "Bonjour "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) string {
+	switch language {
+	case "Chinese":
+		return chinesePrefix
+	case "Spanish":
+		return spanishPrefix
+	case "French":
+		return frenchPrefix
+	default:
+		return englishPrefix
+	}
 }
 
 func main() {
-	fmt.Println(Hello("Fan"))
+	fmt.Println(Hello("Fan", ""))
 }
--- code/learn-go-with-tests/01_hello/hello_test.go
+++ code/learn-go-with-tests/01_hello/hello_test.go
@@ -3,30 +3,57 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-
 	assertCorrectMessage := func(t testing.TB, got, want string) {
-		// t.Helper 的作用是告诉测试框架这个函数是辅助函数，不要报告这个函数的行号。
-		// 比如test不通过，不是报告t.Errorf("got %q, want %q", got, want)的行号，而是对应错误的Run里面的行号
 		t.Helper()
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Fan")
-		want := "Hello Fan"
+	t.Run("in Chinese", func(t *testing.T) {
+		got := Hello("Fan", "Chinese")
+		want := "你好 Fan"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("in English", func(t *testing.T) {
+		got := Hello("Fan", "English")
+		want := "Hello Fan"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello world when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello World"
-
 		assertCorrectMessage(t, got, want)
 	})
 }
+
+// func TestHello(t *testing.T) {
+
+// 	assertCorrectMessage := func(t testing.TB, got, want string) {
+// 		// t.Helper 的作用是告诉测试框架这个函数是辅助函数，不要报告这个函数的行号。
+// 		// 比如test不通过，不是报告t.Errorf("got %q, want %q", got, want)的行号，而是对应错误的Run里面的行号
+// 		t.Helper()
+// 		if got != want {
+// 			t.Errorf("got %q, want %q", got, want)
+// 		}
+// 	}
+
+// 	t.Run("saying hello to people", func(t *testing.T) {
+// 		got := Hello("Fan")
+// 		want := "Hello Fan"
+
+// 		assertCorrectMessage(t, got, want)
+// 	})
+
+// 	t.Run("saying hello world when an empty string is supplied", func(t *testing.T) {
+// 		got := Hello("")
+// 		want := "Hello World"
+
+// 		assertCorrectMessage(t, got, want)
+// 	})
+// }
 
 // func TestHello(t *testing.T) {
 
