6f17b21c7cde9fc9bbf7bfee2a050aba1af36071
fan <291950304@qq.com>
2025-10-20 14:31:18 +0800

    chore: go integers

---
+++ code/learn-go-with-tests/02_integers/adder_test.go
@@ -0,0 +1,21 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// 下面这个注释删除了，函数会被编译，但不会被执行
+	// Output: 6
+}
