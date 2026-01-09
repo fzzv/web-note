8564d2b5c8a6ff3a1fc80a9834a95e5b35f86550
fan <291950304@qq.com>
2026-01-09 17:31:14 +0800

    chore: 实现一个简单的日志拦截器

--- code/go/go-grpc-example/server_unary/server.go
+++ code/go/go-grpc-example/server_unary/server.go
@@ -5,6 +5,7 @@ import (
 	"go-grpc-example/proto"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -20,6 +21,15 @@ func (s *Server) SayHello(ctx context.Context, in *proto.HelloRequest) (*proto.H
 	return &proto.HelloResponse{Message: "Hello, " + in.Name}, nil
 }
 
+// 实现一个简单的日志拦截器
+func loggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	start := time.Now()
+	res, err := handler(ctx, req)
+	duration := time.Since(start)
+	log.Printf("📢 [gRPC 日志] 方法: %s | 耗时: %v | 错误: %v", info.FullMethod, duration, err)
+	return res, err
+}
+
 func main() {
 	// 1. 监听端口
 	lis, err := net.Listen("tcp", ":50051")
@@ -27,7 +37,10 @@ func main() {
 		log.Fatalf("监听端口失败: %v", err)
 	}
 	// 2. 创建 grpc 服务器
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		// 注册拦截器
+		grpc.UnaryInterceptor(loggingInterceptor),
+	)
 	// 3. 注册服务
 	proto.RegisterGreeterServiceServer(s, &Server{})
 	log.Printf("服务正在监听 :50051...")
