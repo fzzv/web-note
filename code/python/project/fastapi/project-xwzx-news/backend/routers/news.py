ba0f78b47f3584e4e51a6bfe13cd9ffdcda7b24a
fan <291950304@qq.com>
2026-08-05 15:52:50 +0800

    chore(fastapi): redis cache news data

--- code/python/project/fastapi/project-xwzx-news/backend/routers/news.py
+++ code/python/project/fastapi/project-xwzx-news/backend/routers/news.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from config.db_conf import get_db
-from crud import news
+from crud import news, news_cache
 
 # 创建 APIRouter 实例
 # prefix 路由前缀（API 接口规范文档）
@@ -19,7 +19,7 @@ router = APIRouter(prefix="/api/news", tags=["news"])
 @router.get("/categories")
 async def get_categories(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
     # 先获取数据库里面新闻分类数据 → 先定义模型类 → 封装查询数据的方法
-    categories = await news.get_categories(db, skip, limit)
+    categories = await news_cache.get_categories(db, skip, limit)
     return {
         "code": 200,
         "message": "获取新闻分类成功",
@@ -36,7 +36,7 @@ async def get_news_list(
 ):
     # 思路：处理分页规则 → 查询新闻列表 → 计算总量 → 计算是否还有更多
     offset = (page - 1) * page_size
-    news_list = await news.get_news_list(db, category_id, offset, page_size)
+    news_list = await news_cache.get_news_list(db, category_id, offset, page_size)
     total = await news.get_news_count(db, category_id)
     # (跳过的 + 当前列表里面的数量) < 总量
     has_more = (offset + len(news_list)) < total
@@ -54,7 +54,7 @@ async def get_news_list(
 @router.get("/detail")
 async def get_news_detail(news_id: int = Query(..., alias="id"), db: AsyncSession = Depends(get_db)):
     # 获取新闻详情 + 浏览量+1 + 相关新闻
-    news_detail = await news.get_news_detail(db, news_id)
+    news_detail = await news_cache.get_news_detail(db, news_id)
     if not news_detail:
         raise HTTPException(status_code=404, detail="新闻不存在")
 
@@ -62,7 +62,7 @@ async def get_news_detail(news_id: int = Query(..., alias="id"), db: AsyncSessio
     if not views_res:
         raise HTTPException(status_code=404, detail="新闻不存在")
 
-    related_news = await news.get_related_news(db, news_detail.id, news_detail.category_id)
+    related_news = await news_cache.get_related_news(db, news_detail.id, news_detail.category_id)
 
     return {
       "code": 200,
